bbb93facf85ae436659ea1fd5ed92fd24a5c21a7
Aamir Alam <aamiralam1991@gmail.com>
2024-12-29 01:03:52 +0530

    fix cors error

--- src/index.js
+++ src/index.js
@@ -4,9 +4,35 @@ const cron = require("node-cron");
 const tokenRoutes = require("./routes/tokenRoutes");
 const listPairRoutes = require("./routes/listPairRoutes");
 const tokenService = require("./services/tokenService");
+var cors = require("cors");
+const cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var path = require("path");
 require("dotenv").config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.static(path.join(__dirname)));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
